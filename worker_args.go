954713afca6d5ada73c709ad5c49bfbf4818e65c
Cariad Eccleston <cariad@cariad.earth>
2025-03-22 13:03:56 +0000

    Add workerArgs.blockWidthF and workerArgs.maxIterationsF

--- worker_args.go
+++ worker_args.go
@@ -9,7 +9,17 @@ import (
 type workerArgs struct {
 	blockChannel chan block
 	blockWidth   int
-	img          *image.RGBA
-	renderArgs   *RenderArgs
-	waitGroup    *sync.WaitGroup
+
+	// The float64 value of blockWidth. This is precalculated to save a million
+	// casts at runtime.
+	blockWidthF float64
+
+	img *image.RGBA
+
+	// The float64 value of the maximum number of iterations to perform. This is
+	// precalculated to save a million casts at runtime.
+	maxIterationsF float64
+
+	renderArgs *RenderArgs
+	waitGroup  *sync.WaitGroup
 }
